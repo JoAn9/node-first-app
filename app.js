e787f7f28753345d7c39ec2bcfc58481c272591a
JoAn <joannacalinska@gmail.com>
2018-11-19 09:05:09 +0100

    node

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const logger = require('./logger');
-// require('./logger.js')
+const path = require('path');
 
-console.log(logger);
+const pathObj = path.parse(__filename);
+
+// console.log(pathObj);
+
+const os = require('os');
+
+// console.log(os.totalmem());
+// console.log(os.freemem());
+
+const fs = require('fs');
+
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+const files = fs.readdir('./', function(err, files) {
+	if (err) console.log('Error', err);
+	else console.log('Result', files);
+});
+console.log(files);
