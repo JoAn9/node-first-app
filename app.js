dc7542afa5775cabd9f10b1ab8a83edefa5fbfbb
JoAn <joannacalinska@gmail.com>
2018-11-20 09:01:26 +0100

    node

--- app.js
+++ app.js
@@ -30,3 +30,21 @@ emitter.on('messageLogged', arg => {
 });
 
 emitter.log('hello');
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	if (req.url === '/') {
+		res.write('Hello You');
+		res.end();
+	}
+
+	if (req.url === '/courses') {
+		res.write(JSON.stringify([1, 2, 3, 4, 5]));
+		res.end();
+	}
+});
+
+server.listen(3000);
+
+console.log('Listening on port 3000');
